fa7eeb93e57e573bf09befb9d205eba72ec113a7
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-04-19 12:11:51 +0530

    Add files via upload

---
+++ Person01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person01;
+
+class Person01 {
+
+public static int pCount; 
+
+public static void main(String[ ] args) { 
+
+   Person01.pCount = 1; 
+
+   Person01.pCount++;
+
+   System.out.println(Person01.pCount); 
+
+  }
+
+}
+
+   
+
